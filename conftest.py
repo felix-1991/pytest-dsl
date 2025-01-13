36d90db73653f5df5255e8ca7d6ab3bf82c7b55a
FelixChen <14407776+felix-1991@user.noreply.gitee.com>
2025-01-13 20:55:04 +0800

    简单demo增加一点tag标识

--- conftest.py
+++ conftest.py
@@ -2,14 +2,12 @@ import pytest
 import allure
 from main import get_lexer, get_parser, execute
 
-def pytest_collect_file(parent, path):
-    if path.ext == ".auto":
-        return AutoFile.from_parent(parent, fspath=path)
-
+def pytest_collect_file(parent, file_path):
+    if file_path.suffix == ".auto":
+        return AutoFile.from_parent(parent, path=file_path)
 
 class AutoFile(pytest.File):
     def collect(self):
-        # Parse metadata
         lexer = get_lexer()
         parser = get_parser()
         
@@ -17,29 +15,46 @@ class AutoFile(pytest.File):
             content = f.read()
             ast = parser.parse(content, lexer=lexer)
             
-            # Find metadata node
             metadata = next((n for n in ast.children if n.type == 'Metadata'), None)
             if metadata:
+                title = None
+                description = None
+                tags = []
                 for item in metadata.children:
                     if item.type == '@name':
-                        allure.dynamic.title(item.value)
+                        title = item.value
                     elif item.type == '@description':
-                        allure.dynamic.description(item.value)
+                        description = item.value
                     elif item.type == '@tags':
-                        for tag in item.value:
-                            allure.tag(tag)
-                            pytest.mark.marker(tag)
+                        tags.extend(tag.value for tag in item.value)
                     elif item.type == '@author':
                         allure.dynamic.label("author", item.value)
                     elif item.type == '@date':
                         allure.dynamic.label("date", item.value)
-        
-        yield AutoItem.from_parent(self, name=self.fspath.basename, ast=ast)
+                
+                item = AutoItem.from_parent(
+                    self,
+                    name=self.fspath.basename,
+                    ast=ast,
+                    title=title,
+                    description=description,
+                    tags=tags
+                )
+                if title:
+                    item.add_marker(pytest.mark.allure_title(title))
+                if description:
+                    item.add_marker(pytest.mark.allure_description(description))
+                for tag in tags:
+                    item.add_marker(pytest.mark.tag(tag))
+                yield item
 
 class AutoItem(pytest.Item):
-    def __init__(self, name, parent, ast):
+    def __init__(self, name, parent, ast, title=None, description=None, tags=None):
         super().__init__(name, parent)
         self.ast = ast
+        self.title = title
+        self.description = description
+        self.tags = tags
 
     def runtest(self):
         with allure.step("Execute test steps"):
